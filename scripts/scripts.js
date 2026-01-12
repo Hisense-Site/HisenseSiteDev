8f99c62180fec954a039b4bb9f47595a8e8924de
LiangYuan <v-liangyuan@mircosoft.com>
2026-01-13 03:31:21 +0800

    add product-spec block

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -146,7 +146,7 @@ async function loadLazy(doc) {
 function loadDelayedImages() {
   const currentHostname = window.location.hostname;
 
-  if (currentHostname.includes('hisensesitedev')) {
+  if (currentHostname.includes('hisensesitedev') || currentHostname.includes('localhost')) {
     const images = document.querySelectorAll('img');
     const domainPrefix = 'https://publish-p174152-e1855821.adobeaemcloud.com';
 
