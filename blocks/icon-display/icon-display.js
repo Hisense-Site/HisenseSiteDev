fdcf47bd3ad3989d5484b132175d988d6616b37b
qi.b.zhang <qi.b.zhang@accenture.com>
2026-01-14 15:28:35 +0800

    feat: update icon display

--- blocks/icon-display/icon-display.js
+++ blocks/icon-display/icon-display.js
@@ -1,3 +1,23 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
 export default function decorate(block) {
-  console.log(block);
+  const ul = document.createElement('ul');
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    moveInstrumentation(row, li);
+    while (row.firstElementChild) li.append(row.firstElementChild);
+    [...li.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) { div.className = 'icon-image'; }
+      else div.className = 'icon-title';
+    });
+    // or use li.
+    ul.append(li);
+  });
+  ul.querySelectorAll('picture > img').forEach((img) => {
+    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '100%' }]);
+    moveInstrumentation(img, optimizedPic.querySelector('img'));
+    img.closest('picture').replaceWith(optimizedPic);
+  });
+  block.replaceChildren(ul);
 }
