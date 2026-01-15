b919616b12389a94dc89b1a8237768948bcd4e3a
shanshanlv <shanshan.lv@accenture.cn>
2026-01-15 14:48:48 +0800

    feat: update banner js

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,27 +1,47 @@
 export default function decorate(block) {
+  console.log(block, 'block');
   [...block.children].forEach((row, idx) => {
+    console.log(row, 'row---block')
     if (idx === 0) {
       row.className = 'pc-box-img';
-      // const btnContainer = document.createElement('div')
-      // const buttonEl = row.querySelectorAll('.button-container')
-      // console.log(buttonEl, 'buttonEl')
-      // btnContainer.append(buttonEl);
-      // console.log([...row.children].length, 'row123');
-      // [...row.children].forEach((column, colIdx) => {
-      //   console.log(colIdx, column, 'colIdx, column')
-      //   switch(colIdx) {
-      //     case 0:
-      //       const pEl = document.querySelector('p');
-      //       pEl.className = 'banner-img'
-      //       console.log(pEl, 'pEl')
-      //       if (pEl.classList.contains('button-container')) {
-      //         pEl.classList.remove('button-container')
-      //       }
-      //       break;
-          
+      // 找到所有 .pc-box 元素中的 div 子元素
+      // const pcBoxChildDivEl = document.querySelector('.pc-box-img > div');
+      // const ctaContainerEl = document.createElement('div')
+      // ctaContainerEl.className = 'cta-container'
+      // console.log(pcBoxChildDivEl, 'pcBoxDivChildren')
+      // const divChildren = Array.from(pcBoxChildDivEl.children);
+      // console.log(divChildren, 'divChildren')
+    // const lastChildrenEl = divChildren[divChildren.length - 1]
+    //   const previousSibling = lastChildrenEl.previousElementSibling;
+    //   if (previousSibling.classList.contains('button-container')) {
+    //     console.log(previousSibling, 'kkkkkjkj')
+    //     previousSibling.className = 'green-btn'
+    //     ctaContainerEl.append(previousSibling, lastChildrenEl)
+    //   } else {
+    //     ctaContainerEl.append(lastChildrenEl)
+    //   }
+    //   // console.log(pcBoxChildDivEl, 'div');
+    //   pcBoxChildDivEl.append(ctaContainerEl)
 
-      //   }
-      // })
+
+    // 遍历获取每个 div 的子元素
+    // pcBoxDivChildren.forEach((div, index) => {
+    //   const children = Array.from(div.children);
+    //   console.log(children);
+    //   // 或者对每个子元素进行操作
+    //   // const lastChildrenEl = children[children.length - 1]
+    //   // const previousSibling = lastChildrenEl.previousElementSibling;
+    //   // if (previousSibling.classList.contains('button-container')) {
+    //   //   console.log(previousSibling, 'kkkkkjkj')
+    //   //   previousSibling.className = 'green-btn'
+    //   //   ctaContainerEl.append(previousSibling, lastChildrenEl)
+    //   // } else {
+    //   //   ctaContainerEl.append(lastChildrenEl)
+    //   // }
+    //   // console.log(div, 'div');
+    //   // div.append(ctaContainerEl)
+    // });
+    // pcBoxDivChildren.append(ctaContainerEl)
     } else {
       row.className = 'mobile-box-img';
     }
