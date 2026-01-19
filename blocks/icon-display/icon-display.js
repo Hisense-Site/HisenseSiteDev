d6ef01d67bec2574392790ea2a392e42fd0577a2
qi.b.zhang <qi.b.zhang@accenture.com>
2026-01-19 19:50:46 +0800

    update icon display

--- blocks/icon-display/icon-display.js
+++ blocks/icon-display/icon-display.js
@@ -2,34 +2,33 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-  console.log(block.children);
-  // const box = document.createElement('div');
-  // box.classList.add('icon-display-box');
-  // [...block.children].forEach((row) => {
-  //   const item = document.createElement('div');
-  //   item.classList.add('icon-display-item');
-  //   moveInstrumentation(row, item);
-  //   while (row.firstElementChild) item.append(row.firstElementChild);
-  //   const text = document.createElement('div');
-  //   text.className = 'icon-display-item-text';
-  //   [...item.children].forEach((div) => {
-  //     if (div.innerHTML.trim() === '') {
-  //       div.remove();
-  //     }
-  //     if (div.querySelector('picture') && div.children.length === 1) { div.className = 'icon-image'; }
-  //     else if (div.children.length === 1) {
-  //       div.className = 'icon-text';
-  //       text.append(div);
-  //     }
-  //   });
-  //   if (text.children.length > 0) item.append(text);
-  //   // or use div.
-  //   box.append(item);
-  // });
-  // box.querySelectorAll('picture > img').forEach((img) => {
-  //   const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ height: '40px' }]);
-  //   moveInstrumentation(img, optimizedPic.querySelector('img'));
-  //   img.closest('picture').replaceWith(optimizedPic);
-  // });
-  // block.replaceChildren(box);
+  const box = document.createElement('div');
+  box.classList.add('icon-display-box');
+  [...block.children].forEach((row) => {
+    const item = document.createElement('div');
+    item.classList.add('icon-display-item');
+    moveInstrumentation(row, item);
+    while (row.firstElementChild) item.append(row.firstElementChild);
+    const text = document.createElement('div');
+    text.className = 'icon-display-item-text';
+    [...item.children].forEach((div) => {
+      if (div.innerHTML.trim() === '') {
+        div.remove();
+      }
+      if (div.querySelector('picture') && div.children.length === 1) { div.className = 'icon-image'; }
+      else if (div.children.length === 1) {
+        div.className = 'icon-text';
+        text.append(div);
+      }
+    });
+    if (text.children.length > 0) item.append(text);
+    // or use div.
+    box.append(item);
+  });
+  box.querySelectorAll('picture > img').forEach((img) => {
+    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ height: '40px' }]);
+    moveInstrumentation(img, optimizedPic.querySelector('img'));
+    img.closest('picture').replaceWith(optimizedPic);
+  });
+  block.replaceChildren(box);
 }
