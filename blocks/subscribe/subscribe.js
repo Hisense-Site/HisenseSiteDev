523fc392b2e8db5d1ef1644115cf0b2263706fd4
XiangqianDeng <xiangqian.deng@accenture.com>
2026-01-27 17:05:11 +0800

    fix jslint issue

--- blocks/subscribe/subscribe.js
+++ blocks/subscribe/subscribe.js
@@ -1,5 +1,5 @@
 export default function decorate(block) {
-  const blockElement = block.getElementsByTagName('div')[0];
+  // const blockElement = block.getElementsByTagName('div')[0];
   // clear existing content
   block.innerHTML = '';
   // subscribe block
