2be24dd724c3d915d738e931bd17446ad848b33e
XiangqianDeng <xiangqian.deng@accenture.com>
2026-01-27 17:02:00 +0800

    add new fields for subscribe block

--- blocks/subscribe/subscribe.js
+++ blocks/subscribe/subscribe.js
@@ -1,4 +1,7 @@
 export default function decorate(block) {
+  const blockElement = block.getElementsByTagName('div')[0];
+  // clear existing content
+  block.innerHTML = '';
   // subscribe block
   block.append(document.createTextNode('Subscribe Block'));
 }
