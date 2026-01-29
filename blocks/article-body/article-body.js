10af9f0b318bcb7d1ef89b43454b84d0f165b36e
avalonXH <hang.xiao@accenture.com>
2026-01-29 18:22:13 +0800

    test

--- blocks/article-body/article-body.js
+++ blocks/article-body/article-body.js
@@ -1,3 +1,16 @@
 export default function decorate(block) {
   console.log(block);
+  [...block.children].forEach((row) => {
+    const type = row.dataset.itemType;
+    const { classes } = row.dataset;
+    console.log(type, classes);
+
+    if (type === 'image') {
+      // image logic
+    }
+
+    if (type === 'quote') {
+      // quote logic
+    }
+  });
 }
