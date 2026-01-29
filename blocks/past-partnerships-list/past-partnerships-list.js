7afc5cda5ca0268b0b1f189cd4652c9d3b671fa4
qi.b.zhang <qi.b.zhang@accenture.com>
2026-01-29 17:50:27 +0800

    update

--- blocks/past-partnerships-list/past-partnerships-list.js
+++ blocks/past-partnerships-list/past-partnerships-list.js
@@ -4,23 +4,29 @@ import { moveInstrumentation } from '../../scripts/scripts.js';
 export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    li.classList.add('card-item');
-    moveInstrumentation(row, li);
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) { div.className = 'card-image'; }
-      else div.className = 'card-body';
-    });
-
-    ul.append(li);
+  const title = document.createElement('div');
+  [...block.children].forEach((row, i) => {
+    if (i === 0) {
+      title.className = 'title';
+      // const div = document.createElement('div');
+      // moveInstrumentation(row, div);
+      title.append(row);
+    } else {
+      const li = document.createElement('li');
+      li.classList.add('card-item');
+      moveInstrumentation(row, li);
+      while (row.firstElementChild) li.append(row.firstElementChild);
+      [...li.children].forEach((div) => {
+        if (div.children.length === 1 && div.querySelector('picture')) div.className = 'card-image';
+        else div.className = 'card-body';
+      });
+      ul.append(li);
+    }
   });
   ul.querySelectorAll('picture > img').forEach((img) => {
     const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
     moveInstrumentation(img, optimizedPic.querySelector('img'));
     img.closest('picture').replaceWith(optimizedPic);
   });
-
-  block.replaceChildren(ul);
+  block.replaceChildren(title, ul);
 }
