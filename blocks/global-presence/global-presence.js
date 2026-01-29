4d96f403816695252f53f3419db6a995486c5d1f
alexxxxxsun <shuangrui.a.sun@accenture.cn>
2026-01-29 16:59:50 +0800

    feat: add global presence js & css

--- blocks/global-presence/global-presence.js
+++ blocks/global-presence/global-presence.js
@@ -1,10 +1,62 @@
-import { debounce } from '../../utils/dom-helper.js';
+import { createElement, debounce } from '../../utils/dom-helper.js';
 import { loadScrollTrigger } from '../../utils/animation-helper.js';
 import { isUniversalEditorAsync } from '../../utils/ue-helper.js';
 
 export default async function decorate(block) {
   // ========== CONSTRUCT DOM [START] ========== //
+  const container = block.querySelector('div:first-child');
+  const elements = container.querySelectorAll('p');
 
+  const contentContainer = createElement('div', 'global-presence-content h-grid-container');
+
+  elements.forEach((row) => {
+    const picture = row.querySelector('picture');
+    if (picture) {
+      picture.classList.add('h-picture');
+      const backgroundImage = createElement('div', 'global-presence-background');
+      backgroundImage.appendChild(picture);
+      block.appendChild(backgroundImage);
+      row.remove();
+    } else {
+      row.classList.add('h-text');
+      contentContainer.appendChild(row);
+    }
+  });
+
+  const textElements = contentContainer.querySelectorAll('.h-text');
+  if (textElements.length > 0) {
+    const title = createElement('h1');
+    title.textContent = textElements[0]?.textContent;
+    textElements[0].replaceWith(title);
+    if (textElements[1]) {
+      contentContainer.appendChild(textElements[1]);
+    }
+  }
+
+  container.remove();
+
+  const statsList = createElement('ul', 'global-presence-stats-list');
+  [...block.children].forEach((row) => {
+    if (row.classList.length !== 0) {
+      return;
+    }
+    const item = row.querySelector('div');
+    if (item.children.length === 2) {
+      const statsItem = createElement('li', 'global-presence-stats-list-item');
+      statsItem.innerHTML = item.innerHTML;
+      statsList.appendChild(statsItem);
+      item.remove();
+    }
+  });
+  contentContainer.appendChild(statsList);
+
+  const contentPicture = block.querySelector('picture:not(.h-picture)');
+  contentPicture.classList.add('h-picture');
+  const contentImage = createElement('div', 'global-presence-image');
+  contentImage.appendChild(contentPicture);
+  contentContainer.appendChild(contentImage);
+
+  block.appendChild(contentContainer);
   // ========== CONSTRUCT DOM [END] ========== //
 
   const isEditing = await isUniversalEditorAsync();
